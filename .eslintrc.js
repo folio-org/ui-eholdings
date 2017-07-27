24f04f64ad5d9b97ebd50e319f0f1802805cfa49
Jeffrey Cherewaty <jeffrey@cherewaty.com>
2017-07-27 14:38:21 -0500

    Set up eslint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  env: {
+    browser: true
+  },
+  rules: {
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* global module */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
