16c145a6ffb2a30e2ec1bd00beb4c50826cc5327
Jeffrey Cherewaty <jeffrey@frontside.io>
2017-11-13 15:30:09 -0600

    Point to sandbox okapi cluster by default

--- demo/stripes.config.js
+++ demo/stripes.config.js
@@ -1,5 +1,14 @@
+const environment = process.env.NODE_ENV;
+let url;
+
+if (environment === 'production') {
+  url = 'https://okapi.frontside.io';
+} else {
+  url = 'https://okapi-sandbox.frontside.io';
+}
+
 module.exports = {
-  okapi: { url: 'https://okapi.frontside.io', tenant: 'fs' },
+  okapi: { url, tenant: 'fs' },
   config: {
     hasAllPerms: true,
     logCategories: ''
