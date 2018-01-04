bd39e4f6beb53637e242a967c3dc656788904fc7
Jeffrey Cherewaty <jeffrey@frontside.io>
2018-01-04 16:26:36 -0600

    Add tenant modules endpoint to mirage

--- mirage/scenarios/no-backend.js
+++ mirage/scenarios/no-backend.js
@@ -2,5 +2,6 @@ export default function noBackendScenario(server) {
   let ns = server.namespace;
   server.namespace = '';
   server.get('/_/proxy/modules', []);
+  server.get('_/proxy/tenants/:id/modules', []);
   server.namespace = ns;
 }
