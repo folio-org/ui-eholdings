c572fd60b482f08ff8f10c091c3d1abd9f0d0b9a
Wil Wilsman <wil@frontside.io>
2018-03-08 12:43:36 -0600

    Add style imports to test harness
    These imports are normally found in the webpack config used by
    stripes-core, but when running tests using karma-webpack the entry
    field is discarded. By adding them to our test harness, we can ensure
    they will be included in our tests as well.

--- tests/harness.js
+++ tests/harness.js
@@ -10,6 +10,10 @@ import { discoverServices } from '@folio/stripes-core/src/discoverServices';
 import gatherActions from '@folio/stripes-core/src/gatherActions';
 import { setOkapiReady } from '@folio/stripes-core/src/okapiActions';
 
+// load these in our tests
+import 'typeface-source-sans-pro';
+import '@folio/stripes-components/lib/global.css';
+
 import Root from '@folio/stripes-core/src/components/Root';
 
 const actionNames = gatherActions();
