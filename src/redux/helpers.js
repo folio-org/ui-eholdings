f9b640cd5bb778c0fd9c0f1b22b4aea46a6f6810
Joe LaSala <joe@frontside.io>
2018-04-05 17:46:37 -0400

    Use a fold to instead of iterating incoming data.  Introduces funcadelic.

--- src/redux/helpers.js
+++ src/redux/helpers.js
@@ -1,7 +1,18 @@
+import { foldl, append } from 'funcadelic';
+
 /**
  * Helper to merge incoming `relationship` information non-
  * destructively.  Relationship data held in the store will not
  * be overwritten with empty data: only by new data.
+ *
+ * NOTE: for now, this is specific to `relationship` keys in the store, and so
+ * it may seem strange that it lives in a generic 'helpers' file all by itself.
+ * I do not make cuts before they're required, but it seems imminent that this
+ * pattern will be applied to other areas of the store.  Pulling in the FP
+ * power of Funcadelic here will allow us to extract common logical operations like
+ * this one and genericize them to work against complex data structures like
+ * the store.
+ *
  * @param {Object} existing - `relationship` data from a record in the store
  * @param {Object} incoming - `relationship` data from an incoming request
  * @returns {Object} safely merged relationship data fit to be persisted
@@ -11,15 +22,11 @@
 export function mergeRelationships(existing, incoming) {
   if (!incoming) { return existing; }
 
-  let mergedRelationships = {};
-
-  for (let recordType in incoming) {
-    if (incoming[recordType] && incoming[recordType].data) {
-      mergedRelationships[recordType] = incoming[recordType];
+  return foldl((relationships, { key: recordType, value: relationshipData }) => {
+    if (relationshipData && relationshipData.data) {
+      return append(relationships, { [recordType]: relationshipData });
     } else {
-      mergedRelationships[recordType] = existing[recordType] || {};
+      return append(relationships, { [recordType]: existing[recordType] || {} });
     }
-  }
-
-  return mergedRelationships;
+  }, {}, incoming);
 }
