b6f7c13f665e8b9b65f28904a41eee3b30b4626e
Jeffrey Cherewaty <jeffrey@frontside.io>
2018-04-09 13:27:34 -0500

    Hide title management toggle for custom packages

--- src/components/package-show/package-show.js
+++ src/components/package-show/package-show.js
@@ -139,7 +139,7 @@ export default class PackageShow extends Component {
           )}
           bodyContent={(
             <div>
-              <DetailsViewSection label="Package information">
+              <DetailsViewSection label={model.isCustom ? 'Custom package information' : 'Managed package information'}>
                 <KeyValue label="Provider">
                   <div data-test-eholdings-package-details-provider>
                     <Link to={`/eholdings/providers/${model.providerId}`}>{model.providerName}</Link>
@@ -214,7 +214,7 @@ export default class PackageShow extends Component {
                 )}
               </DetailsViewSection>
               <DetailsViewSection label="Title management">
-                {packageSelected ? (
+                {packageSelected && !model.isCustom ? (
                   <div>
                     {packageAllowedToAddTitles != null ? (
                       <div>
