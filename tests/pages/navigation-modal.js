4415af25e8bf98277a872420b62eec7edf37bc3c
Jeffrey Cherewaty <jeffrey@frontside.io>
2018-04-30 13:02:12 -0500

    Remove unecessary use of isVisible

--- tests/pages/navigation-modal.js
+++ tests/pages/navigation-modal.js
@@ -1,11 +1,9 @@
 import {
   interactor,
-  isVisible,
   clickable
 } from '@bigtest/interactor';
 
 @interactor class NavigationModal {
-  isModalVisible = isVisible();
   clickContinue = clickable('[data-test-navigation-modal-continue]');
   clickDismiss = clickable('[data-test-navigation-modal-dismiss]');
 }
