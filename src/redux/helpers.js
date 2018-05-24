ba62a6c90697f25047d2d6a9427c33000b1f8e23
Wil Wilsman <wil@frontside.io>
2018-05-24 10:49:26 -0500

    Merge attributes for resolved requests

--- src/redux/helpers.js
+++ src/redux/helpers.js
@@ -5,20 +5,10 @@ import { foldl, append } from 'funcadelic';
  * destructively.  Relationship data held in the store will not
  * be overwritten with empty data: only by new data.
  *
- * NOTE: for now, this is specific to `relationship` keys in the store, and so
- * it may seem strange that it lives in a generic 'helpers' file all by itself.
- * I do not make cuts before they're required, but it seems imminent that this
- * pattern will be applied to other areas of the store.  Pulling in the FP
- * power of Funcadelic here will allow us to extract common logical operations like
- * this one and genericize them to work against complex data structures like
- * the store.
- *
  * @param {Object} existing - `relationship` data from a record in the store
  * @param {Object} incoming - `relationship` data from an incoming request
  * @returns {Object} safely merged relationship data fit to be persisted
  */
-
-// eslint-disable-next-line import/prefer-default-export
 export function mergeRelationships(existing, incoming) {
   if (!incoming) { return existing; }
 
@@ -29,4 +19,19 @@ export function mergeRelationships(existing, incoming) {
       return append(relationships, { [recordType]: existing[recordType] || {} });
     }
   }, {}, incoming);
+}
+
+/**
+ * Helper to merge incoming `attribute` information non-
+ * destructively. If an attribute is defined, it will be written to
+ * the store; if undefined, the store's value (if any) will persist.
+ *
+ * @param {Object} existing - attributes from a record in the store
+ * @param {Object} incoming - attributes from an incoming request
+ * @returns {Object} safely merged attributes fit to be persisted
+ */
+export function mergeAttributes(existing, incoming) {
+  if (!incoming) { return existing; }
+
+  return append(existing, incoming);
 }
