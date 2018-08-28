bbda2281ef1cd106721ec7f4c27cfc0f1421b9b8
carolegodfrey <cgodfrey@ebsco.com>
2018-08-28 16:46:35 -0400

    Update samples for mirage

--- bigtest/network/factories/token.js
+++ bigtest/network/factories/token.js
@@ -1,7 +1,5 @@
 import { Factory, faker } from '@bigtest/mirage';
 
-let helpText = '<ul><li>Enter your Gale<sup></sup> site ID in the space provided below. The site ID may contain a combination of alpha/numeric characters, varying in length. <blockquote><p> Example: The site ID immediately follows /itweb/ in a URL. The site ID in the following URL is …………….</ul>';
-
 export default Factory.extend({
   factName: () => faker.random.arrayElement([
     '[[mysiteid]]',
@@ -14,9 +12,9 @@ export default Factory.extend({
     '/test3/'
   ]),
   helpText: () => faker.random.arrayElement([
-    '\u003cul\u003e\r\n    \u003cli\u003eEnter your Test\u003csup\u003e®\u003c/sup\u003e site ID in the space provided below. The site ID may contain a combination of alpha/numeric characters, varying in length. \u003cblockquote style=\\"margin-right: 0px;\\" dir=\\"ltr\\"\u003e\r\n    \u003cp\u003e Example: The site ID immediately follows /itweb/ in a URL. The site ID in the following URL is \u003ci\u003eaa11bb22\u003c/i\u003e. \u003c/p\u003e\r\n    \u003c/blockquote\u003e\u003c/li\u003e\r\n\u003c/ul\u003e\r\n\u003cblockquote style=\\"margin-right: 0px;\\"',
-    helpText,
-    'http://test.com/authentication'
+    '<ul><li>Enter your Gale Token</li></ul>',
+    '<a>http://test.com/authentication</a>',
+    '<ul><li>Get Help Here</li></ul>'
   ]),
   value: () => faker.random.arrayElement([
     null,
