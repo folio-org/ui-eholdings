963eec9d101e016bcec9391d7e825e00d6614213
carolegodfrey <cgodfrey@ebsco.com>
2018-09-21 17:11:37 -0400

    Update accordion list header to show Over if number of list items is 10,000

--- src/components/details-view/accordion-list-header.js
+++ src/components/details-view/accordion-list-header.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FormattedNumber, injectIntl, intlShape } from 'react-intl';
+import { FormattedMessage, FormattedNumber, injectIntl, intlShape } from 'react-intl';
 import { KeyValue } from '@folio/stripes-components';
 import AccordionHeader from '@folio/stripes-components/lib/Accordion/headers/DefaultAccordionHeader';
 import styles from './accordion-list-header.css';
 
 function AccordionListHeader(props) {
   let { intl } = props;
+  let showOver = props.resultsLength === 10000;
+
   return (
     <div className={styles['accordion-list-header']}>
       <AccordionHeader {...props} />
       {props.open && (
         <KeyValue label={intl.formatMessage({ id: 'ui-eholdings.label.accordionList' })}>
           <div data-test-eholdings-details-view-results-count>
+            {showOver && (
+              <span>
+                <FormattedMessage id="ui-eholdings.over" />
+                &nbsp;
+              </span>
+            )}
             <FormattedNumber value={props.resultsLength} />
           </div>
         </KeyValue>
