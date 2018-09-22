06d692ae37973ea1b5746924601b154f53498b6d
Jeffrey Cherewaty <jeffrey@frontside.io>
2018-09-22 18:42:32 -0500

    Lint

--- src/components/package-search-list.js
+++ src/components/package-search-list.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withRouter } from 'react-router';
 import { FormattedMessage } from 'react-intl';
 
 import QueryList from './query-list';
