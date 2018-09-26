adbf6443ad22791c539bfbd1168739246a38b137
carolegodfrey <cgodfrey@ebsco.com>
2018-09-26 13:32:30 -0400

    Removed no longer used meatlength

--- src/components/package/show/package-show.js
+++ src/components/package/show/package-show.js
@@ -420,7 +420,6 @@ class PackageShow extends Component {
           listSectionId="packageShowTitles"
           onListToggle={this.handleSectionToggle}
           resultsLength={model.resources.length}
-          metaLength={model.titleCount}
           renderList={scrollable => (
             <QueryList
               type="package-titles"
