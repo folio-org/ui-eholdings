73e94b0e2c5f0d18d07204d969c6f1db85987dfb
carolegodfrey <cgodfrey@ebsco.com>
2018-09-27 10:47:56 -0400

    Update veendor to provider in documentation

--- src/components/details-view/accordion-list-header.js
+++ src/components/details-view/accordion-list-header.js
@@ -9,7 +9,7 @@ function AccordionListHeader(props) {
   let { intl } = props;
   // RM API does not return exact number of results when count is over 10K
   // For title lists, resultsLength of 10000 indicates this.
-  // For other lists (package and vendor) resultsLength of 10001 indicates this.
+  // For other lists (package and provider) resultsLength of 10001 indicates this.
   let overCount = props.listType === 'titles' ? 10000 : 10001;
   let showOver = props.resultsLength === overCount;
   let displayOverCount = 10000;
