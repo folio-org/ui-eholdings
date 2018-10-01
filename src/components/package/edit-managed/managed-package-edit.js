49cca929043d5fb3fca36ac4304695aba35a72c7
Jeffrey Cherewaty <jeffrey@frontside.io>
2018-10-01 15:18:48 -0500

    Fork RadioButton and RadioButtonGroup

--- src/components/package/edit-managed/managed-package-edit.js
+++ src/components/package/edit-managed/managed-package-edit.js
@@ -7,10 +7,10 @@ import {
   Headline,
   Icon,
   Modal,
-  ModalFooter,
-  RadioButton,
-  RadioButtonGroup
+  ModalFooter
 } from '@folio/stripes-components';
+import RadioButton from '../../radio-button';
+import RadioButtonGroup from '../../radio-button-group';
 import { processErrors } from '../../utilities';
 
 import DetailsView from '../../details-view';
