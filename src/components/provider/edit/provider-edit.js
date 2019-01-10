41a7f9b1b45fabce06a26436618e3025949e4110
Jeffrey Cherewaty <jeffrey@frontside.io>
2019-01-10 13:33:34 -0600

    Use React Final Form for provider edit

--- src/components/provider/edit/provider-edit.js
+++ src/components/provider/edit/provider-edit.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
-import { reduxForm } from 'redux-form';
+import { Form } from 'react-final-form';
 import { FormattedMessage } from 'react-intl';
 import {
   Button,
@@ -13,22 +13,20 @@ import DetailsView from '../../details-view';
 import DetailsViewSection from '../../details-view-section';
 import NavigationModal from '../../navigation-modal';
 import Toaster from '../../toaster';
-import ProxySelectField from '../../proxy-select';
-import TokenField from '../../token';
+import ProxySelectField from '../_fields/proxy-select';
+import TokenField from '../_fields/token';
 import PaneHeaderButton from '../../pane-header-button';
 import FullViewLink from '../../full-view-link';
 
-class ProviderEdit extends Component {
+export default class ProviderEdit extends Component {
   static propTypes = {
     fullViewLink: PropTypes.oneOfType([
       PropTypes.string,
       PropTypes.object
     ]),
-    handleSubmit: PropTypes.func,
     model: PropTypes.object.isRequired,
     onCancel: PropTypes.func.isRequired,
     onSubmit: PropTypes.func.isRequired,
-    pristine: PropTypes.bool,
     proxyTypes: PropTypes.object.isRequired,
     rootProxy: PropTypes.object.isRequired
   };
@@ -64,84 +62,85 @@ class ProviderEdit extends Component {
       model,
       proxyTypes,
       rootProxy,
-      handleSubmit,
       onSubmit,
-      pristine,
     } = this.props;
 
     let supportsTokens = model.providerToken && model.providerToken.prompt;
     let hasTokenValue = model.providerToken && model.providerToken.value;
 
     return (
-      <Fragment>
-        <Toaster toasts={processErrors(model)} position="bottom" />
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <DetailsView
-            type="provider"
-            model={model}
-            key={model.id}
-            paneTitle={model.name}
-            actionMenu={this.getActionMenu}
-            lastMenu={(
-              <Fragment>
-                {model.update.isPending && (
-                <Icon icon="spinner-ellipsis" />
-                )}
-                <PaneHeaderButton
-                  disabled={pristine || model.update.isPending}
-                  type="submit"
-                  buttonStyle="primary"
-                  data-test-eholdings-provider-save-button
-                >
-                  {model.update.isPending ?
-                    (<FormattedMessage id="ui-eholdings.saving" />)
-                    :
-                    (<FormattedMessage id="ui-eholdings.save" />)}
-                </PaneHeaderButton>
-              </Fragment>
-          )}
-            bodyContent={(
-              <Fragment>
-                <DetailsViewSection
-                  label={<FormattedMessage id="ui-eholdings.provider.providerSettings" />}
-                >
-                  {model.packagesSelected > 0 ? (
-                    <div>
-                      {(!proxyTypes.request.isResolved || !rootProxy.request.isResolved) ? (
-                        <Icon icon="spinner-ellipsis" />
+      <Form
+        onSubmit={onSubmit}
+        initialValues={{
+          proxyId: model.proxy.id,
+          providerTokenValue: model.providerToken.value
+        }}
+        render={({ handleSubmit, pristine }) => (
+          <Fragment>
+            <Toaster toasts={processErrors(model)} position="bottom" />
+            <form onSubmit={handleSubmit}>
+              <DetailsView
+                type="provider"
+                model={model}
+                key={model.id}
+                paneTitle={model.name}
+                actionMenu={this.getActionMenu}
+                lastMenu={(
+                  <Fragment>
+                    {model.update.isPending && (
+                    <Icon icon="spinner-ellipsis" />
+                    )}
+                    <PaneHeaderButton
+                      disabled={pristine || model.update.isPending}
+                      type="submit"
+                      buttonStyle="primary"
+                      data-test-eholdings-provider-save-button
+                    >
+                      {model.update.isPending ?
+                        (<FormattedMessage id="ui-eholdings.saving" />)
+                        :
+                        (<FormattedMessage id="ui-eholdings.save" />)}
+                    </PaneHeaderButton>
+                  </Fragment>
+              )}
+                bodyContent={(
+                  <Fragment>
+                    <DetailsViewSection
+                      label={<FormattedMessage id="ui-eholdings.provider.providerSettings" />}
+                    >
+                      {model.packagesSelected > 0 ? (
+                        <div>
+                          {(!proxyTypes.request.isResolved || !rootProxy.request.isResolved) ? (
+                            <Icon icon="spinner-ellipsis" />
+                          ) : (
+                            <div data-test-eholdings-provider-proxy-select>
+                              <ProxySelectField proxyTypes={proxyTypes} inheritedProxyId={rootProxy.data.attributes.proxyTypeId} />
+                            </div>
+                          )}
+
+                          {supportsTokens && (
+                            <fieldset>
+                              <Headline tag="legend">
+                                <FormattedMessage id="ui-eholdings.provider.token" />
+                              </Headline>
+                              <TokenField token={model.providerToken} tokenValue={hasTokenValue} type="provider" />
+                            </fieldset>
+                          )}
+                        </div>
                       ) : (
-                        <div data-test-eholdings-provider-proxy-select>
-                          <ProxySelectField proxyTypes={proxyTypes} inheritedProxyId={rootProxy.data.attributes.proxyTypeId} />
+                        <div data-test-eholdings-provider-package-not-selected>
+                          <FormattedMessage id="ui-eholdings.provider.noPackagesSelected" />
                         </div>
                       )}
-
-                      {supportsTokens && (
-                        <fieldset>
-                          <Headline tag="legend">
-                            <FormattedMessage id="ui-eholdings.provider.token" />
-                          </Headline>
-                          <TokenField token={model.providerToken} tokenValue={hasTokenValue} type="provider" />
-                        </fieldset>
-                      )}
-                    </div>
-                  ) : (
-                    <div data-test-eholdings-provider-package-not-selected>
-                      <FormattedMessage id="ui-eholdings.provider.noPackagesSelected" />
-                    </div>
-                  )}
-                </DetailsViewSection>
-              </Fragment>
-            )}
-          />
-        </form>
-        <NavigationModal when={!pristine && !model.update.isPending} />
-      </Fragment>
+                    </DetailsViewSection>
+                  </Fragment>
+                )}
+              />
+            </form>
+            <NavigationModal when={!pristine && !model.update.isPending} />
+          </Fragment>
+        )}
+      />
     );
   }
 }
-
-export default reduxForm({
-  enableReinitialize: true,
-  form: 'ProviderEdit',
-  destroyOnUnmount: false,
-})(ProviderEdit);
