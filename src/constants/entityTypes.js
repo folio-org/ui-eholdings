b0da51ae894c1877ec05d0916b3b0be6f70c1a0c
Maksym Dryha <43514877+MaksymDryha@users.noreply.github.com>
2019-05-27 12:29:14 +0300

    create "create note" page (#799)

---
+++ src/constants/entityTypes.js
@@ -0,0 +1,8 @@
+const entityTypes = {
+  PROVIDER: 'provider',
+  RESOURCE: 'resource',
+  TITLE: 'title',
+  PACKAGE: 'package',
+};
+
+export default entityTypes;
--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ import TitleEdit from './routes/title-edit';
 import TitleCreate from './routes/title-create';
 import ResourceShow from './routes/resource-show';
 import ResourceEdit from './routes/resource-edit';
+import NoteCreate from './routes/note-create';
 
 import SettingsKnowledgeBaseRoute from './routes/settings-knowledge-base';
 import SettingsRootProxyRoute from './routes/settings-root-proxy';
@@ -66,6 +67,7 @@ class EHoldings extends Component {
             <Route path={`${rootPath}/titles/:titleId/edit`} exact component={TitleEdit} />
             <Route path={`${rootPath}/resources/:id`} exact component={ResourceShow} />
             <Route path={`${rootPath}/resources/:id/edit`} exact component={ResourceEdit} />
+            <Route path={`${rootPath}/notes/new`} exact component={NoteCreate} />
             <Route render={() => (<Redirect to={`${rootPath}?searchType=providers`} />)} />
           </Switch>
         </Route>
