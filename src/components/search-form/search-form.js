b714684cd5bc67ed0d8908eaa1288b16fdb17b48
carolegodfrey <cgodfrey@ebsco.com>
2019-06-12 09:00:08 -0400

    Limit tag filter to provider only

--- src/components/search-form/search-form.js
+++ src/components/search-form/search-form.js
@@ -287,7 +287,7 @@ class SearchForm extends Component {
                 activeFilters={combinedFilters}
                 onUpdate={this.handleUpdateFilter}
               />
-              {this.renderTagFilter()}
+              {(searchType === searchTypes.PROVIDERS) && this.renderTagFilter()}
             </div>
           )}
         </form>
