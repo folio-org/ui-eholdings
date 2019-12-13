facb879af5946ed1fd82bb3b17d3afc3306b5742
Igor Godlevskyi <igor_godlevskyi@epam.com>
2019-12-13 14:58:16 +0200

    Added another test file

--- test/bigtest/tests/custom-package-edit-selection-test.js
+++ test/bigtest/tests/custom-package-edit-selection-test.js
@@ -5,7 +5,7 @@ import setupApplication from '../helpers/setup-application';
 import PackageShowPage from '../interactors/package-show';
 import PackageEditPage from '../interactors/package-edit';
 
-describe.skip('CustomPackageEditSelection', function () {
+describe('CustomPackageEditSelection', function () {
   setupApplication();
   let provider,
     providerPackage;
