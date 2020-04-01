c5dbfb33449f84795cf40cdfe0226f9f8dec9cdf
Denys Bohdan <Denys_Bohdan@epam.com>
2020-04-01 18:52:26 +0300

    UIEH-869 Created basic implementation

--- src/components/utilities.js
+++ src/components/utilities.js
@@ -213,3 +213,9 @@ export function formatCustomLabelsValues(formValues) {
 
   return formattedFormValues;
 }
+
+export const isKnowledgeBaseConfigured = (knowledgeBase) => {
+  const { name, apiKey, url, customerId } = knowledgeBase.attributes;
+
+  return !!name && !!apiKey && !!url && !!customerId;
+};
