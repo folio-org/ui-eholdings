131e2c26e3f066e1140812a984a36f1dbbaff82b
Maksym Dryha <Maksym_Dryha@epam.com>
2020-05-13 10:48:12 +0300

    handle error when trying to delete access status type that doesn't exist

--- src/redux/actions/delete-access-type-failure.js
+++ src/redux/actions/delete-access-type-failure.js
@@ -1,8 +1,11 @@
 export const DELETE_ACCESS_TYPE_FAILURE = 'DELETE_ACCESS_TYPE_FAILURE';
 
-export function deleteAccessTypeFailure({ errors }) {
+export function deleteAccessTypeFailure({ errors, accessTypeId }) {
   return {
     type: DELETE_ACCESS_TYPE_FAILURE,
-    payload: errors,
+    payload: {
+      errors,
+      accessTypeId,
+    },
   };
 }
