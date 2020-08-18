59c7cc6cbb47135178173c945bde0dfea6f5c4e6
Yelyzaveta Khokhlova <yelyzaveta_khokhlova@epam.com>
2020-08-18 15:37:41 +0300

    UIEH-905 fixed import

--- src/components/no-results-message/noResultsMessage.js
+++ src/components/no-results-message/noResultsMessage.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 import { Icon } from '@folio/stripes/components';
 
-import css from './NoResultsMessage.css';
+import css from './noResultsMessage.css';
 
 const propTypes = {
   children: PropTypes.node.isRequired,
