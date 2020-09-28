9d82e7bbc6db382c4a8b6f425a316665609c816b
vlavel.well <vladyslav_velytskyi@epam.com>
2020-09-28 16:20:54 +0300

    UIEH-971: Fix issue in agreements accordion with displaying start date

--- src/components/agreements-list/agreements-list.js
+++ src/components/agreements-list/agreements-list.js
@@ -11,6 +11,8 @@ import {
   Icon,
 } from '@folio/stripes/components';
 
+import { TIME_ZONE } from '../../constants';
+
 
 const COLUMN_NAMES = ['startDate', 'status', 'name'];
 const COLUMN_WIDTHS = {
@@ -78,6 +80,7 @@ export default class AgreementsList extends React.Component {
               year="numeric"
               month="numeric"
               day="numeric"
+              timeZone={TIME_ZONE}
             />
           ),
           status: agreementStatus.label,
