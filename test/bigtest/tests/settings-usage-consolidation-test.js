d35badd5e78281893077ee192d23f130290beeab
Denys Bohdan <Denys_Bohdan@epam.com>
2020-10-28 13:57:04 +0200

    UIEH-969 Added tests

--- test/bigtest/tests/settings-usage-consolidation-test.js
+++ test/bigtest/tests/settings-usage-consolidation-test.js
@@ -41,6 +41,62 @@ describe('With usage consolidation available to a customer', () => {
     it('should show correct saved start month value', () => {
       expect(SettingsUsageConsolidationPage.usageConsolidationStartMonthField.value).to.equal('mar');
     });
+
+    it('should show usage consolidation platform type field', () => {
+      expect(SettingsUsageConsolidationPage.usageConsolidationPlatformTypeField.isPresent).to.be.true;
+    });
+
+    it('should show correct saved platform type value', () => {
+      expect(SettingsUsageConsolidationPage.usageConsolidationPlatformTypeField.value).to.equal('publisher');
+    });
+
+    describe('when changing start month value', () => {
+      beforeEach(() => {
+        return SettingsUsageConsolidationPage.usageConsolidationStartMonthField.selectAndBlur('April');
+      });
+
+      it('should enable the save button', () => {
+        expect(SettingsUsageConsolidationPage.saveButtonDisabled).to.be.false;
+      });
+
+      describe('and clicking save', () => {
+        beforeEach(() => {
+          return SettingsUsageConsolidationPage.save();
+        });
+
+        it('should save the new value', () => {
+          expect(SettingsUsageConsolidationPage.usageConsolidationStartMonthField.value).to.equal('apr');
+        });
+
+        it('should disable the save button', () => {
+          expect(SettingsUsageConsolidationPage.saveButtonDisabled).to.be.true;
+        });
+      });
+    });
+
+    describe('when changing platform type value', () => {
+      beforeEach(() => {
+        return SettingsUsageConsolidationPage.usageConsolidationPlatformTypeField.selectAndBlur('Non-publisher platforms only');
+      });
+
+      it('should enable the save button', () => {
+        expect(SettingsUsageConsolidationPage.saveButtonDisabled).to.be.false;
+      });
+
+      describe('and clicking save', () => {
+        beforeEach(() => {
+          return SettingsUsageConsolidationPage.save();
+        });
+
+        it('should save the new value', () => {
+          expect(SettingsUsageConsolidationPage.usageConsolidationPlatformTypeField.value).to.equal('nonPublisher');
+        });
+
+        it('should disable the save button', () => {
+          expect(SettingsUsageConsolidationPage.saveButtonDisabled).to.be.true;
+        });
+      });
+    });
   });
 
   describe('when usage consolidation has not been set up', () => {
@@ -51,8 +107,12 @@ describe('With usage consolidation available to a customer', () => {
       await wait(1000);
     });
 
-    it('should show default value as January', () => {
+    it('should show default start month value as January', () => {
       expect(SettingsUsageConsolidationPage.usageConsolidationStartMonthField.value).to.equal('jan');
+    });
+
+    it('should show default platform type value as All', () => {
+      expect(SettingsUsageConsolidationPage.usageConsolidationPlatformTypeField.value).to.equal('all');
     });
   });
 });
