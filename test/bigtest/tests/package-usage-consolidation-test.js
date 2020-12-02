0422d07ba558afdbc75eaa36e2172102003ae8ff
vlavel.well <vladyslav_velytskyi@epam.com>
2020-12-02 02:36:48 +0200

    Fix lint issues

--- test/bigtest/tests/package-usage-consolidation-test.js
+++ test/bigtest/tests/package-usage-consolidation-test.js
@@ -4,7 +4,7 @@ import { expect } from 'chai';
 import setupApplication, { axe } from '../helpers/setup-application';
 import PackageShowPage from '../interactors/package-show';
 
-describe.only('PackageShowUsageConsolidation', () => {
+describe('PackageShowUsageConsolidation', () => {
   setupApplication();
   let provider;
   let providerPackage;
@@ -82,7 +82,7 @@ describe.only('PackageShowUsageConsolidation', () => {
         expect(PackageShowPage.usageConsolidation.accordion.isOpen).to.be.true;
       });
 
-      describe.only('when clicking View', () => {
+      describ('when clicking View', () => {
         beforeEach(async () => {
           await PackageShowPage.usageConsolidation.filters.clickView();
           await PackageShowPage.usageConsolidation.content.whenLoaded();
