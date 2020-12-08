ae07cb0d6948e95e7d05753a53a0e6864302ae69
Dmitriy-Litvinenko <Dmitriy_Litvinenko@epam.com>
2020-12-08 15:42:09 +0200

    UIEH-993: Add info popover for Usage Consolidation Accordion (#1200)

--- test/bigtest/interactors/package-usage-consolidation.js
+++ test/bigtest/interactors/package-usage-consolidation.js
@@ -6,12 +6,15 @@ import {
 import { AccordionInteractor } from '@folio/stripes-components/lib/Accordion/tests/interactor';
 
 import UsageConsolidationFilters from './usage-consolidation-filters';
+import UsageConsolidationInfoPopover from './usage-consolidation-info-popover';
 import UsageConsolidationContent from './usage-consolidation-content-package';
 
 export default @interactor class PackageUsageConsolidation {
   accordion = new AccordionInteractor('#packageShowUsageConsolidation');
   isAccordionPresent = isPresent('#packageShowUsageConsolidation');
+
   filters = new UsageConsolidationFilters();
+  infoPopover = new UsageConsolidationInfoPopover();
   content = new UsageConsolidationContent();
 
   whenLoaded() {
--- test/bigtest/interactors/resource-usage-consolidation.js
+++ test/bigtest/interactors/resource-usage-consolidation.js
@@ -6,6 +6,7 @@ import {
 import { AccordionInteractor } from '@folio/stripes-components/lib/Accordion/tests/interactor';
 
 import UsageConsolidationFilters from './usage-consolidation-filters';
+import UsageConsolidationInfoPopover from './usage-consolidation-info-popover';
 import UsageConsolidationContent from './usage-consolidation-content-resource';
 
 export default @interactor class ResourceUsageConsolidation {
@@ -13,6 +14,7 @@ export default @interactor class ResourceUsageConsolidation {
   isAccordionPresent = isPresent('#resourceShowUsageConsolidation');
 
   filters = new UsageConsolidationFilters();
+  infoPopover = new UsageConsolidationInfoPopover();
   content = new UsageConsolidationContent();
 
   whenLoaded() {
