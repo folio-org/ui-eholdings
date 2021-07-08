2ca72ded28b8a3c4c23d4833f8c7fd685e71028d
Vladyslav Velytskyi <55701515+Vladyslav-Velytskyi@users.noreply.github.com>
2021-07-08 14:23:40 +0300

    UIEH-1055: Add tests for AgreementsList (#1371)
    Co-authored-by: Denys Bohdan <denys_bohdan@epam.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,7 @@
 * Fix The Eholdings app is NOT displayed in the app bar when the "eHoldings: Can view Usage..." permission is applied. (UIEH-1142)
 * Add tests for ResourceEditCustomTitle component. (UIEH-1095)
 * Add tests for TitleShow component. (UIEH-1098)
+* Add tests for AgreementsList component. (UIEH-1055)
 
 ## [6.1.0] (https://github.com/folio-org/ui-eholdings/tree/v6.1.0) (2021-06-04)
 * Add tests coverage for keyboard shortcuts to eholdings. (UIEH-1043)
---
+++ src/components/agreements-list/agreements-list.test.js
@@ -0,0 +1,70 @@
+import {
+  fireEvent,
+  render,
+} from '@testing-library/react';
+import noop from 'lodash/noop';
+
+import Harness from '../../../test/jest/helpers/harness';
+import AgreementsList from './agreements-list';
+
+jest.mock('@folio/stripes/components', () => ({
+  ...jest.requireActual('@folio/stripes/components'),
+  FormattedDate: () => ({ value }) => <span>{value}</span>,
+}));
+
+const agreements = {
+  isLoading: false,
+  isUnassigned: false,
+  items: [{
+    agreementStatus: {
+      label: 'label',
+    },
+    id: 'id',
+    name: 'name',
+    startDate: '2021-07-01',
+  }],
+};
+
+describe('Given AgreementsList', () => {
+  const renderAgreementsList = (props) => render(
+    <Harness>
+      <AgreementsList
+        agreements={agreements}
+        onUnassignAgreement={noop}
+        {...props}
+      />
+    </Harness>
+  );
+
+  describe('when click on trash icon', () => {
+    it('should handle onUnassignAgreement', () => {
+      const mockOnUnassignAgreement = jest.fn();
+      const { items: [{ id, name, agreementStatus }] } = agreements;
+      const deletedAgreement = {
+        id,
+        name,
+        rowIndex: 0,
+        status: agreementStatus.label,
+      };
+
+      const { container } = renderAgreementsList({ onUnassignAgreement: mockOnUnassignAgreement });
+
+      fireEvent.click(container.querySelector('button[icon="trash"]'));
+
+      expect(mockOnUnassignAgreement).toHaveBeenCalledWith(expect.objectContaining(deletedAgreement));
+    });
+  });
+
+  describe('when record is on load', () => {
+    it('should show spinner', () => {
+      const { container } = renderAgreementsList({
+        agreements: {
+          ...agreements,
+          isLoading: true,
+        },
+      });
+
+      expect(container.querySelector('.icon-spinner-ellipsis')).toBeDefined();
+    });
+  });
+});
