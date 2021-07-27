146f00f6a50808d979817433b630ae3dcb217899
Mariia Sychova <84023879+mariiaschv@users.noreply.github.com>
2021-07-27 12:04:14 +0300

    UIEH-1066: Jest+RTL > Test ContributorField (#1393)
    * UIEH-1066 Add tests for ContributorField component
    
    * UIEH-1066 Fix constants file naming

--- src/components/title/_fields/contributor/contributor-field.js
+++ src/components/title/_fields/contributor/contributor-field.js
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import { Field } from 'react-final-form';
 import { FieldArray } from 'react-final-form-arrays';
 
@@ -12,131 +11,135 @@ import {
 } from '@folio/stripes/components';
 import { FormattedMessage } from 'react-intl';
 
+import { CONTRIBUTOR_FIELD_VALUE_MAX_LENGTH } from '../../../../constants';
+
 const contributorTypeOptions = [
   {
     value: 'author',
-    translationIdEnding: 'author'
+    translationIdEnding: 'author',
   },
   {
     value: 'editor',
-    translationIdEnding: 'editor'
+    translationIdEnding: 'editor',
   },
   {
     value: 'illustrator',
-    translationIdEnding: 'illustrator'
+    translationIdEnding: 'illustrator',
   },
 ];
 
-export default class ContributorField extends Component {
-  validateName(value) {
-    let errors;
+const validateName = (value) => {
+  let errors;
 
-    if (!value) {
-      errors = <FormattedMessage id="ui-eholdings.validate.errors.contributor.empty" />;
-    }
-
-    if (value && value.length >= 250) {
-      errors = <FormattedMessage id="ui-eholdings.validate.errors.contributor.exceedsLength" />;
-    }
-
-    return errors;
+  if (!value) {
+    errors = <FormattedMessage id="ui-eholdings.validate.errors.contributor.empty" />;
   }
 
-  addNewField(fields) {
-    const defaultContributorTypeOption = contributorTypeOptions[0].value;
-
-    fields.push({ type: defaultContributorTypeOption });
+  if (value?.length >= CONTRIBUTOR_FIELD_VALUE_MAX_LENGTH) {
+    errors = <FormattedMessage id="ui-eholdings.validate.errors.contributor.exceedsLength" />;
   }
 
-  renderContributorTypeField(contributor) {
-    return (
-      <Col
-        md
-        xs={12}
-        data-test-eholdings-contributor-type
+  return errors;
+};
+
+const addNewField = (fields) => {
+  const defaultContributorTypeOption = contributorTypeOptions[0].value;
+
+  fields.push({ type: defaultContributorTypeOption });
+};
+
+const renderContributorTypeOptions = () => {
+  return contributorTypeOptions.map(({ value, translationIdEnding }) => (
+    <FormattedMessage id={`ui-eholdings.label.${translationIdEnding}`}>
+      {message => <option value={value}>{message}</option>}
+    </FormattedMessage>
+  ));
+};
+
+const renderContributorTypeField = (contributor) => {
+  return (
+    <Col
+      md
+      xs={12}
+      data-test-eholdings-contributor-type
+    >
+      <Field
+        name={`${contributor}.type`}
+        component={Select}
+        autoFocus
+        label={<FormattedMessage id="ui-eholdings.type" />}
+        id={`${contributor}-type`}
       >
-        <Field
-          name={`${contributor}.type`}
-          component={Select}
-          autoFocus
-          label={<FormattedMessage id="ui-eholdings.type" />}
-          id={`${contributor}-type`}
-          validate={this.validateContributorType}
-        >
-          {this.renderContributorTypeOptions()}
-        </Field>
-      </Col>
-    );
-  }
+        {renderContributorTypeOptions()}
+      </Field>
+    </Col>
+  );
+};
 
-  renderContributorTypeOptions() {
-    return contributorTypeOptions.map(({ value, translationIdEnding }) => (
-      <FormattedMessage id={`ui-eholdings.label.${translationIdEnding}`}>
-        {message => <option value={value}>{message}</option>}
-      </FormattedMessage>
-    ));
-  }
+const renderContributorNameField = (contributor) => {
+  return (
+    <Col
+      md
+      xs={12}
+      data-test-eholdings-contributor-contributor
+    >
+      <Field
+        name={`${contributor}.contributor`}
+        type="text"
+        id={`${contributor}-input`}
+        component={TextField}
+        label={<FormattedMessage id="ui-eholdings.name" />}
+        validate={validateName}
+      />
+    </Col>
+  );
+};
 
-  renderContributorNameField(contributor) {
-    return (
-      <Col
-        md
-        xs={12}
-        data-test-eholdings-contributor-contributor
-      >
-        <Field
-          name={`${contributor}.contributor`}
-          type="text"
-          id={`${contributor}-input`}
-          component={TextField}
-          label={<FormattedMessage id="ui-eholdings.name" />}
-          validate={this.validateName}
-        />
-      </Col>
-    );
-  }
+const renderFields = (contributor) => {
+  return (
+    <Row>
+      {renderContributorTypeField(contributor)}
+      {renderContributorNameField(contributor)}
+    </Row>
+  );
+};
 
-  renderFields = (contributor) => {
-    return (
-      <Row>
-        {this.renderContributorTypeField(contributor)}
-        {this.renderContributorNameField(contributor)}
-      </Row>
-    );
-  }
+const ContributorField = () => {
+  const addLabel = <FormattedMessage id="ui-eholdings.title.contributor.addContributor" />;
 
-  render() {
-    const addLabel = <FormattedMessage id="ui-eholdings.title.contributor.addContributor" />;
+  const legend = (
+    <Headline tag="h4">
+      <FormattedMessage id="ui-eholdings.label.contributors" />
+    </Headline>
+  );
 
-    const legend = (
-      <Headline tag="h4">
-        <FormattedMessage id="ui-eholdings.label.contributors" />
-      </Headline>
-    );
+  return (
+    <div
+      data-test-eholdings-contributors-fields
+      data-testid="contributor-field"
+    >
+      <FieldArray name="contributors">
+        {({ fields, meta: { initial } }) => {
+          const areInitialValuesPassed = initial?.length;
+          const emptyMessage = areInitialValuesPassed
+            ? <FormattedMessage id="ui-eholdings.title.contributor.notSet" />
+            : '';
 
-    return (
-      <div data-test-eholdings-contributors-fields>
-        <FieldArray name="contributors">
-          {({ fields, meta: { initial } }) => {
-            const areInitialValuesPassed = initial && initial.length;
-            const emptyMessage = areInitialValuesPassed
-              ? <FormattedMessage id="ui-eholdings.title.contributor.notSet" />
-              : '';
+          return (
+            <RepeatableField
+              addLabel={addLabel}
+              emptyMessage={emptyMessage}
+              fields={fields}
+              legend={legend}
+              onAdd={() => addNewField(fields)}
+              onRemove={fields.remove}
+              renderField={renderFields}
+            />
+          );
+        }}
+      </FieldArray>
+    </div>
+  );
+};
 
-            return (
-              <RepeatableField
-                addLabel={addLabel}
-                emptyMessage={emptyMessage}
-                fields={fields}
-                legend={legend}
-                onAdd={() => this.addNewField(fields)}
-                onRemove={fields.remove}
-                renderField={this.renderFields}
-              />
-            );
-          }}
-        </FieldArray>
-      </div>
-    );
-  }
-}
+export default ContributorField;
