1d9be09fb26a3fead219175358d824c1a706f9f3
Mariia Sychova <mariia_sychova@epam.com>
2021-07-27 18:20:51 +0300

    UIEH-1067 Add tests for IdentifiersFields component

--- src/components/title/_fields/identifiers/identifiers-fields.js
+++ src/components/title/_fields/identifiers/identifiers-fields.js
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import { Field } from 'react-final-form';
 import { FieldArray } from 'react-final-form-arrays';
 
@@ -13,96 +12,101 @@ import {
 
 import { FormattedMessage } from 'react-intl';
 
-export default class IdentifiersFields extends Component {
-  validateId(value) {
-    let errors;
+import { IDENTIFIERS_FIELDS_VALUE_MAX_LENGTH } from '../../../../constants';
 
-    if (!value) {
-      errors = <FormattedMessage id="ui-eholdings.validate.errors.identifiers.noBlank" />;
-    }
+const validateId = (value) => {
+  let errors;
 
-    if (value && value.length >= 20) {
-      errors = <FormattedMessage id="ui-eholdings.validate.errors.identifiers.exceedsLength" />;
-    }
-
-    return errors;
+  if (!value) {
+    errors = <FormattedMessage id="ui-eholdings.validate.errors.identifiers.noBlank" />;
   }
 
-  renderField = (identifier) => {
-    return (
-      <Row>
-        <Col
-          md
-          xs={12}
-          data-test-eholdings-identifiers-fields-type
+  if (value?.length >= IDENTIFIERS_FIELDS_VALUE_MAX_LENGTH) {
+    errors = <FormattedMessage id="ui-eholdings.validate.errors.identifiers.exceedsLength" />;
+  }
+
+  return errors;
+};
+
+const renderField = (identifier) => {
+  return (
+    <Row>
+      <Col
+        md
+        xs={12}
+        data-test-eholdings-identifiers-fields-type
+      >
+        <Field
+          name={`${identifier}.flattenedType`}
+          type="text"
+          component={Select}
+          autoFocus={Object.keys(identifier).length === 0}
+          label={<FormattedMessage id="ui-eholdings.type" />}
         >
-          <Field
-            name={`${identifier}.flattenedType`}
-            type="text"
-            component={Select}
-            autoFocus={Object.keys(identifier).length === 0}
-            label={<FormattedMessage id="ui-eholdings.type" />}
-          >
-            <FormattedMessage id="ui-eholdings.label.identifier.issnOnline">
-              {(message) => <option value="0">{message}</option>}
-            </FormattedMessage>
-            <FormattedMessage id="ui-eholdings.label.identifier.issnPrint">
-              {(message) => <option value="1">{message}</option>}
-            </FormattedMessage>
-            <FormattedMessage id="ui-eholdings.label.identifier.isbnOnline">
-              {(message) => <option value="2">{message}</option>}
-            </FormattedMessage>
-            <FormattedMessage id="ui-eholdings.label.identifier.isbnPrint">
-              {(message) => <option value="3">{message}</option>}
-            </FormattedMessage>
-          </Field>
-        </Col>
-        <Col
-          md
-          xs={12}
-          data-test-eholdings-identifiers-fields-id
-        >
-          <FormattedMessage id="ui-eholdings.id">
-            {(fieldName) => (
-              <Field
-                name={`${identifier}.id`}
-                type="text"
-                component={TextField}
-                label={fieldName}
-                validate={this.validateId}
-                ariaLabel={fieldName}
-              />
-            )}
+          <FormattedMessage id="ui-eholdings.label.identifier.issnOnline">
+            {(message) => <option value="0">{message}</option>}
           </FormattedMessage>
-        </Col>
-      </Row>
-    );
-  }
-
-  render() {
-    return (
-      <div data-test-eholdings-identifiers-fields>
-        <FieldArray name="identifiers">
-          {({ fields, meta: { initial } }) => (
-            <RepeatableField
-              addLabel={<FormattedMessage id="ui-eholdings.title.identifier.addIdentifier" />}
-              emptyMessage={
-                initial && initial.length > 0 && initial[0].id ?
-                  <FormattedMessage id="ui-eholdings.title.identifier.notSet" /> : ''
-              }
-              fields={fields}
-              legend={
-                <Headline tag="h4">
-                  <FormattedMessage id="ui-eholdings.label.identifiers" />
-                </Headline>
-              }
-              onAdd={() => fields.push({})}
-              onRemove={index => fields.remove(index)}
-              renderField={this.renderField}
+          <FormattedMessage id="ui-eholdings.label.identifier.issnPrint">
+            {(message) => <option value="1">{message}</option>}
+          </FormattedMessage>
+          <FormattedMessage id="ui-eholdings.label.identifier.isbnOnline">
+            {(message) => <option value="2">{message}</option>}
+          </FormattedMessage>
+          <FormattedMessage id="ui-eholdings.label.identifier.isbnPrint">
+            {(message) => <option value="3">{message}</option>}
+          </FormattedMessage>
+        </Field>
+      </Col>
+      <Col
+        md
+        xs={12}
+        data-test-eholdings-identifiers-fields-id
+      >
+        <FormattedMessage id="ui-eholdings.id">
+          {(fieldName) => (
+            <Field
+              name={`${identifier}.id`}
+              type="text"
+              component={TextField}
+              label={fieldName}
+              validate={validateId}
+              ariaLabel={fieldName}
             />
           )}
-        </FieldArray>
-      </div>
-    );
-  }
-}
+        </FormattedMessage>
+      </Col>
+    </Row>
+  );
+};
+
+const IdentifiersFields = () => {
+  return (
+    <div
+      data-test-eholdings-identifiers-fields
+      data-testid="identifiers-fields"
+    >
+      <FieldArray name="identifiers">
+        {({ fields, meta: { initial } }) => (
+          <RepeatableField
+            addLabel={<FormattedMessage id="ui-eholdings.title.identifier.addIdentifier" />}
+            emptyMessage={
+              initial?.length > 0 && initial[0].id ?
+                <FormattedMessage id="ui-eholdings.title.identifier.notSet" /> : ''
+            }
+            fields={fields}
+            legend={
+              <Headline tag="h4">
+                <FormattedMessage id="ui-eholdings.label.identifiers" />
+              </Headline>
+            }
+            onAdd={() => fields.push({})}
+            onRemove={index => fields.remove(index)}
+            renderField={renderField}
+          />
+        )}
+      </FieldArray>
+    </div>
+  );
+};
+
+export default IdentifiersFields;
