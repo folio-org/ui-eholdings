6e60e42eb80810d6de814315225334decc954c31
Mariia Sychova <mariia_sychova@epam.com>
2021-08-03 15:10:01 +0300

    UIEH-1159 Change Package Settings display on View Package details record

--- src/components/package/show/components/package-settings/package-settings.js
+++ src/components/package/show/components/package-settings/package-settings.js
@@ -1,7 +1,5 @@
 import PropTypes from 'prop-types';
-import {
-  FormattedMessage,
-} from 'react-intl';
+import { FormattedMessage } from 'react-intl';
 import hasIn from 'lodash/fp/hasIn';
 
 import {
@@ -9,17 +7,21 @@ import {
   Headline,
   KeyValue,
   Icon,
+  Row,
+  Col,
 } from '@folio/stripes/components';
 
 import ProxyDisplay from '../../../../proxy-display';
 import TokenDisplay from '../../../../token-display';
 import AccessTypeDisplay from '../../../../access-type-display';
 
-import {
-  getAccessTypeId,
-} from '../../../../utilities';
+import { getAccessTypeId } from '../../../../utilities';
+
 import {
   accessTypesReduxStateShape,
+  MAX_COLUMN_WIDTH,
+  ONE_THIRD_OF_COLUMN_WIDTH,
+  QUARTER_OF_COLUMN_WIDTH,
 } from '../../../../../constants';
 
 const propTypes = {
@@ -50,89 +52,109 @@ const PackageSettings = ({
     const hasPackageToken = hasIn('packageToken.prompt', model);
     const isProxyAvailable = hasProxy && proxyTypes.request.isResolved && model.isLoaded && provider.isLoaded;
     const haveAccessTypesLoaded = !accessStatusTypes?.isLoading && !model.isLoading;
+    const isAccessStatusTypes = accessStatusTypes?.items?.data?.length > 0;
+
+    const packageSettingsColumnWidth = isAccessStatusTypes
+      ? MAX_COLUMN_WIDTH
+      : ONE_THIRD_OF_COLUMN_WIDTH;
+
+    const proxyAndAccessStatusTypeColumnWidth = isAccessStatusTypes
+      ? QUARTER_OF_COLUMN_WIDTH
+      : MAX_COLUMN_WIDTH;
 
     return (
       <>
-        <KeyValue label={<FormattedMessage id="ui-eholdings.package.visibility" />}>
-          <div data-test-eholdings-package-details-visibility-status>
-            {
-              !model.visibilityData.isHidden
-                ? <FormattedMessage id="ui-eholdings.yes" />
-                : <FormattedMessage id="ui-eholdings.package.visibility.no" values={{ visibilityMessage }} />
-            }
-          </div>
-        </KeyValue>
-        {
-          !model.isCustom && (
-            <KeyValue label={<FormattedMessage id="ui-eholdings.package.packageAllowToAddTitles" />}>
-              {
-                packageAllowedToAddTitles !== null
+        <Row>
+          <Col xs>
+            <Row>
+              <Col xs={6}>
+                <KeyValue label={<FormattedMessage id="ui-eholdings.package.visibility" />}>
+                  <div data-test-eholdings-package-details-visibility-status>
+                    {!model.visibilityData.isHidden
+                      ? <FormattedMessage id="ui-eholdings.yes" />
+                      : <FormattedMessage
+                        id="ui-eholdings.package.visibility.no"
+                        values={{ visibilityMessage }}
+                      />
+                    }
+                  </div>
+                </KeyValue>
+              </Col>
+              {!model.isCustom && (
+                <Col xs={6}>
+                  <KeyValue label={<FormattedMessage id="ui-eholdings.package.packageAllowToAddTitles" />}>
+                    {packageAllowedToAddTitles !== null
+                      ? (
+                        <div data-test-eholdings-package-details-allow-add-new-titles>
+                          {packageAllowedToAddTitles
+                            ? <FormattedMessage id="ui-eholdings.yes" />
+                            : <FormattedMessage id="ui-eholdings.no" />
+                          }
+                        </div>
+                      )
+                      : <Icon icon="spinner-ellipsis" />
+                    }
+                  </KeyValue>
+                </Col>
+              )}
+            </Row>
+          </Col>
+          <Col xs={packageSettingsColumnWidth}>
+            <Row>
+              <Col xs={proxyAndAccessStatusTypeColumnWidth}>
+                {isProxyAvailable
                   ? (
-                    <div data-test-eholdings-package-details-allow-add-new-titles>
-                      {
-                        packageAllowedToAddTitles
-                          ? <FormattedMessage id="ui-eholdings.yes" />
-                          : <FormattedMessage id="ui-eholdings.no" />
-                      }
-                    </div>
+                    <ProxyDisplay
+                      proxy={model.proxy}
+                      proxyTypesRecords={proxyTypes.resolver.state.proxyTypes.records}
+                      inheritedProxyId={provider?.proxy?.id || ''}
+                    />
                   )
                   : <Icon icon="spinner-ellipsis" />
-              }
-            </KeyValue>
-          )
-        }
-        {
-          isProxyAvailable
-            ? (
-              <ProxyDisplay
-                proxy={model.proxy}
-                proxyTypesRecords={proxyTypes.resolver.state.proxyTypes.records}
-                inheritedProxyId={provider?.proxy?.id || ''}
-              />
-            )
-            : <Icon icon="spinner-ellipsis" />
-        }
-        {
-          <div data-test-eholdings-access-type>
-            {haveAccessTypesLoaded
-              ? (
-                <AccessTypeDisplay
-                  accessTypeId={getAccessTypeId(model)}
-                  accessStatusTypes={accessStatusTypes}
+                }
+              </Col>
+              {isAccessStatusTypes && (
+                <Col xsOffset={3} xs={proxyAndAccessStatusTypeColumnWidth}>
+                  <div data-test-eholdings-access-type>
+                    {haveAccessTypesLoaded
+                      ? (
+                        <AccessTypeDisplay
+                          accessTypeId={getAccessTypeId(model)}
+                          accessStatusTypes={accessStatusTypes}
+                        />
+                      )
+                      : <Icon icon="spinner-ellipsis" />
+                    }
+                  </div>
+                </Col>
+              )}
+            </Row>
+          </Col>
+        </Row>
+        {hasProviderToken && (
+          provider.isLoading
+            ? <Icon icon="spinner-ellipsis" />
+            : (
+              <KeyValue label={<FormattedMessage id="ui-eholdings.provider.token" />}>
+                <TokenDisplay
+                  token={provider.providerToken}
+                  type="provider"
                 />
-              )
-              : <Icon icon="spinner-ellipsis" />
-            }
-          </div>
-        }
-        {
-          hasProviderToken && (
-            provider.isLoading
-              ? <Icon icon="spinner-ellipsis" />
-              : (
-                <KeyValue label={<FormattedMessage id="ui-eholdings.provider.token" />}>
-                  <TokenDisplay
-                    token={provider.providerToken}
-                    type="provider"
-                  />
-                </KeyValue>
-              )
-          )
-        }
-        {
-          hasPackageToken && (
-            model.isLoading
-              ? <Icon icon="spinner-ellipsis" />
-              : (
-                <KeyValue label={<FormattedMessage id="ui-eholdings.package.token" />}>
-                  <TokenDisplay
-                    token={model.packageToken}
-                    type="package"
-                  />
-                </KeyValue>
-              )
-          )
-        }
+              </KeyValue>
+            )
+        )}
+        {hasPackageToken && (
+          model.isLoading
+            ? <Icon icon="spinner-ellipsis" />
+            : (
+              <KeyValue label={<FormattedMessage id="ui-eholdings.package.token" />}>
+                <TokenDisplay
+                  token={model.packageToken}
+                  type="package"
+                />
+              </KeyValue>
+            )
+        )}
       </>
     );
   };
@@ -151,10 +173,9 @@ const PackageSettings = ({
       id="packageShowSettings"
       onToggle={onToggle}
     >
-      {
-        packageSelected
-          ? renderPackageSettings()
-          : <p><FormattedMessage id="ui-eholdings.package.visibility.notSelected" /></p>
+      {packageSelected
+        ? renderPackageSettings()
+        : <p><FormattedMessage id="ui-eholdings.package.visibility.notSelected" /></p>
       }
     </Accordion>
   );
