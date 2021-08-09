a44f3de79649377c299cdf6ceaf282a56aaa3abd
Denys Bohdan <denys_bohdan@epam.com>
2021-08-09 15:35:44 +0300

    UIEH-1075 Jest+RTL > Test SearchFilters (#1409)
    * UIEH-1075 Jest+RTL > Test SearchFilters
    
    * UIEH-1075 fix comments

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -35,6 +35,7 @@
 * Add tests for PackageSearchList, ProviderSearchList, TitleSearchList components. (UIEH-1070)
 * Change `loadMoreButton` to `PrevNextButtons`. (UIEH-1163)
 * Add tests for PackageCreate component. (UIEH-1072)
+* Jest+RTL > Test SearchFilters. (UIEH-1075)
 
 ## [6.1.1] (https://github.com/folio-org/ui-eholdings/tree/v6.1.1) (2021-07-15)
 * Fix: saving the update of coverage settings temporarily shows the old date. (UIEH-1146)
---
+++ src/components/search-form/search-filters.test.js
@@ -0,0 +1,58 @@
+import {
+  render,
+  cleanup,
+  fireEvent,
+} from '@testing-library/react';
+
+import Harness from '../../../test/jest/helpers/harness';
+import SearchFilters from './search-filters';
+
+const mockOnUpdate = jest.fn();
+
+const renderSearchFilters = (props = {}) => render(
+  <Harness>
+    <SearchFilters
+      activeFilters={{
+        sort: 'undefined',
+      }}
+      availableFilters={[{
+        label: 'Sort-label',
+        name: 'sort',
+        defaultValue: 'relevance',
+        options: [{
+          label: 'Relevance-label',
+          value: 'relevance',
+        }, {
+          label: 'Package-label',
+          value: 'package',
+        }],
+      }]}
+      onUpdate={mockOnUpdate}
+      searchType="packages"
+      {...props}
+    />
+  </Harness>
+);
+
+describe('Given SearchFilters', () => {
+  afterEach(() => {
+    cleanup();
+    mockOnUpdate.mockClear();
+  });
+
+  it('should render available filters', () => {
+    const { getByText } = renderSearchFilters();
+
+    expect(getByText('Sort-label')).toBeDefined();
+  });
+
+  describe('when editing filter value', () => {
+    it('should call onUpdate', () => {
+      const { getByLabelText } = renderSearchFilters();
+
+      fireEvent.click(getByLabelText('Package-label'));
+
+      expect(mockOnUpdate).toBeCalledWith({ sort: 'package' });
+    });
+  });
+});
