26d90061fd4f35839629c04f097bf6cbb0238955
Mariia Sychova <mariia_sychova@epam.com>
2021-08-16 18:31:08 +0300

    UIEH-1078 Add tests for SettingsRootProxy component

--- src/components/settings/settings-root-proxy/settings-root-proxy.js
+++ src/components/settings/settings-root-proxy/settings-root-proxy.js
@@ -1,4 +1,7 @@
-import { Component } from 'react';
+import {
+  useEffect,
+  useState,
+} from 'react';
 import PropTypes from 'prop-types';
 import { Form } from 'react-final-form';
 import createFocusDecorator from 'final-form-focus';
@@ -11,85 +14,98 @@ import { rootProxy as rootProxyShapes } from '../../../constants';
 
 const focusOnErrors = createFocusDecorator();
 
-export default class SettingsRootProxy extends Component {
-  static propTypes = {
-    onSubmit: PropTypes.func.isRequired,
-    proxyTypes: PropTypes.object.isRequired,
-    rootProxy: rootProxyShapes.RootProxyReduxStateShape.isRequired,
-  };
+const SettingsRootProxy = ({
+  onSubmit,
+  proxyTypes,
+  rootProxy,
+}) => {
+  const [toasts, setToasts] = useState([]);
 
-  state = {
-    toasts: [],
-  }
-
-  componentDidUpdate(prevProps) {
-    const { rootProxy } = this.props;
-
+  useEffect(() => {
     if (rootProxy.isUpdated) {
-      // eslint-disable-next-line react/no-did-update-set-state
-      this.setState(({ toasts }) => ({
-        toasts: [...toasts, {
-          id: `root-proxy-${rootProxy.id}`,
-          message: <FormattedMessage id="ui-eholdings.settings.rootProxy.updated" />,
-          type: 'success'
-        }],
-      }));
+      setToasts(prevToasts => {
+        return [
+          ...prevToasts,
+          {
+            id: `root-proxy-${rootProxy.id}`,
+            message: <FormattedMessage id="ui-eholdings.settings.rootProxy.updated" />,
+            type: 'success',
+          },
+        ];
+      });
     }
+  }, [rootProxy]);
 
-    if (prevProps.rootProxy.errors !== rootProxy.errors) {
-      // eslint-disable-next-line react/no-did-update-set-state
-      this.setState(({ toasts }) => ({
-        toasts: [...toasts, ...rootProxy.errors.map(error => ({
-          id: `root-proxy-${rootProxy.id}`,
-          message: error.title,
-          type: 'error'
-        }))],
-      }));
+  useEffect(() => {
+    if (rootProxy.errors.length > 0) {
+      setToasts(prevToasts => {
+        return [
+          ...prevToasts,
+          ...rootProxy.errors.map(error => ({
+            id: `root-proxy-${rootProxy.id}`,
+            message: error.title,
+            type: 'error',
+          })),
+        ];
+      });
     }
-  }
+  }, [rootProxy]);
 
-  render() {
-    const {
-      rootProxy,
-      proxyTypes,
-      onSubmit,
-    } = this.props;
-
-    return (
-      <Form
-        onSubmit={onSubmit}
-        initialValues={{
-          rootProxyServer: rootProxy.data?.attributes?.proxyTypeId
-        }}
-        decorators={[focusOnErrors]}
-        render={(formState) => (
-          <SettingsForm
-            data-test-eholdings-settings-root-proxy
-            id="root-proxy-form"
-            formState={formState}
-            updateIsPending={rootProxy.isLoading}
-            title={<FormattedMessage id="ui-eholdings.settings.rootProxy" />}
-            toasts={this.state.toasts}
-          >
-            {proxyTypes.isLoading ? (
+  return (
+    <Form
+      onSubmit={onSubmit}
+      initialValues={{
+        rootProxyServer: rootProxy.data?.attributes?.proxyTypeId,
+      }}
+      decorators={[focusOnErrors]}
+      render={(formState) => (
+        <SettingsForm
+          data-test-eholdings-settings-root-proxy
+          data-testid="settings-root-proxy-form"
+          id="root-proxy-form"
+          formState={formState}
+          updateIsPending={rootProxy.isLoading}
+          title={<FormattedMessage id="ui-eholdings.settings.rootProxy" />}
+          toasts={toasts}
+        >
+          {proxyTypes.isLoading
+            ? (
               <Icon icon="spinner-ellipsis" />
-            ) : (
+            )
+            : (
               <div data-test-eholdings-settings-root-proxy-select>
                 <RootProxySelectField
                   proxyTypes={proxyTypes}
                   value={formState.values.rootProxyServer}
                 />
               </div>
-            )}
+            )
+          }
 
-            <p><FormattedMessage id="ui-eholdings.settings.rootProxy.ebsco.customer.message" /></p>
+          <p><FormattedMessage id="ui-eholdings.settings.rootProxy.ebsco.customer.message" /></p>
 
-            <p>
-              <FormattedMessage id="ui-eholdings.settings.rootProxy.warning" />
-            </p>
-          </SettingsForm>
-        )}
-      />
-    );
-  }
-}
+          <p><FormattedMessage id="ui-eholdings.settings.rootProxy.warning" /></p>
+        </SettingsForm>
+      )}
+    />
+  );
+};
+
+SettingsRootProxy.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  proxyTypes: PropTypes.shape({
+    errors: PropTypes.array.isRequired,
+    isFailed: PropTypes.bool.isRequired,
+    isLoading: PropTypes.bool.isRequired,
+    items: PropTypes.arrayOf(PropTypes.shape({
+      attributes: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+      }).isRequired,
+      id: PropTypes.string.isRequired,
+    })),
+  }).isRequired,
+  rootProxy: rootProxyShapes.RootProxyReduxStateShape.isRequired,
+};
+
+export default SettingsRootProxy;
