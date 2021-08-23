0f7e6a1beb85d60c947ee454c663d37d778f5a2b
Vladyslav Velytskyi <Vladyslav_Velytskyi@epam.com>
2021-08-23 13:43:14 +0300

    UIEH-1086: Add tests for SearchForm component

---
+++ src/components/search-form/components/tags-filter-accordion/tags-filter-accordion.js
@@ -0,0 +1,115 @@
+import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
+
+import {
+  Accordion,
+  Icon,
+} from '@folio/stripes/components';
+import { MultiSelectionFilter } from '@folio/stripes/smart-components';
+
+import SearchByCheckbox from '../search-by-checkbox';
+
+import styles from './tags-filter-accordion.css';
+
+const propTypes = {
+  dataOptions: PropTypes.arrayOf(PropTypes.shape({
+    label: PropTypes.string,
+    value: PropTypes.string,
+  })).isRequired,
+  handleStandaloneFilterChange: PropTypes.func.isRequired,
+  header: PropTypes.node.isRequired,
+  isOpen: PropTypes.bool,
+  onStandaloneFilterChange: PropTypes.func.isRequired,
+  onStandaloneFilterToggle: PropTypes.func.isRequired,
+  onToggle: PropTypes.func.isRequired,
+  searchByTagsEnabled: PropTypes.bool.isRequired,
+  searchFilter: PropTypes.object,
+  tagsModel: PropTypes.object.isRequired,
+};
+
+const defaultProps = {
+  isOpen: false,
+  searchFilter: {},
+};
+
+const TagsFilterAccordion = ({
+  tagsModel,
+  searchByTagsEnabled,
+  searchFilter,
+  onStandaloneFilterChange,
+  onStandaloneFilterToggle,
+  isOpen,
+  onToggle,
+  header,
+  handleStandaloneFilterChange,
+  dataOptions,
+}) => {
+  const {
+    tags = '',
+  } = searchFilter;
+
+  let tagsList = [];
+
+  if (tags) {
+    tagsList = Array.isArray(tags)
+      ? tags
+      : tags.split(',');
+  }
+
+  tagsList = tagsList.map(tag => tag.toLowerCase()).sort();
+
+  return tagsModel.isLoading
+    ? <Icon icon="spinner-ellipsis" />
+    : (
+      <div
+        data-test-eholdings-tag-filter
+        role="tab"
+      >
+        <Accordion
+          label={<FormattedMessage id="ui-eholdings.tags" />}
+          id="accordionTagFilter"
+          separator={false}
+          open={isOpen}
+          closedByDefault
+          header={header}
+          displayClearButton={tagsList.length > 0}
+          onClearFilter={() => onStandaloneFilterChange({ tags: undefined })}
+          onToggle={onToggle}
+          className={styles['search-filter-accordion']}
+        >
+          <span
+            className="sr-only"
+            id="selectTagFilter-label"
+          >
+            <FormattedMessage id="ui-eholdings.tags" />
+          </span>
+          <SearchByCheckbox
+            filterType="tags"
+            isEnabled={searchByTagsEnabled}
+            onStandaloneFilterToggle={onStandaloneFilterToggle}
+          />
+          <FormattedMessage id="ui-eholdings.tags.filter">
+            {
+              label => (
+                <MultiSelectionFilter
+                  id="selectTagFilter"
+                  ariaLabel={label}
+                  dataOptions={dataOptions}
+                  name="tags"
+                  onChange={handleStandaloneFilterChange}
+                  selectedValues={tagsList}
+                  disabled={!searchByTagsEnabled}
+                  aria-labelledby="selectTagFilter-label"
+                />
+              )
+            }
+          </FormattedMessage>
+        </Accordion>
+      </div>
+    );
+};
+
+TagsFilterAccordion.propTypes = propTypes;
+TagsFilterAccordion.defaultProps = defaultProps;
+
+export default TagsFilterAccordion;
---
+++ src/components/search-form/components/tags-filter-accordion/tags-filter-accordion.test.js
@@ -0,0 +1,70 @@
+import {
+  render,
+} from '@testing-library/react';
+import noop from 'lodash/noop';
+
+import TagsFilterAccordion from './tags-filter-accordion';
+
+const tagsModel = {
+  isLoading: false,
+};
+
+const renderTagsFilterAccordion = (props = {}) => render(
+  <TagsFilterAccordion
+    tagsModel={tagsModel}
+    searchByTagsEnabled
+    onStandaloneFilterChange={noop}
+    onStandaloneFilterToggle={noop}
+    isOpen
+    header={() => <span>Accordion Header</span>}
+    dataOptions={[{
+      label: 'opt1',
+      value: 'opt1',
+    }, {
+      label: 'opt2',
+      value: 'opt1',
+    }]}
+    handleStandaloneFilterChange={noop}
+    onToggle={noop}
+    {...props}
+  />
+);
+
+describe('Given TagsFilterAccordion', () => {
+  describe('when selected some items', () => {
+    it('should show clear filters button when tags is string', () => {
+      const { container } = renderTagsFilterAccordion({
+        isOpen: false,
+        searchFilter: {
+          tags: 'opt1,opt2',
+        },
+      });
+
+      expect(container.querySelector('[icon=times-circle-solid]')).toBeDefined();
+    });
+
+    it('should show clear filters button when tags is array', () => {
+      const { container } = renderTagsFilterAccordion({
+        isOpen: false,
+        searchFilter: {
+          tags: ['opt1', 'opt2'],
+        },
+      });
+
+      expect(container.querySelector('[icon=times-circle-solid]')).toBeDefined();
+    });
+  });
+
+  describe('when tags data is loading', () => {
+    it('should show spinner', () => {
+      const { container } = renderTagsFilterAccordion({
+        tagsModel: {
+          ...tagsModel,
+          isLoading: true,
+        },
+      });
+
+      expect(container.querySelector('.icon-spinner-ellipsis')).toBeDefined();
+    });
+  });
+});
