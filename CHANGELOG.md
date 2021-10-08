1a7819868df6222999473d706472bd22cacf0159
Mariia Sychova <84023879+mariiaschv@users.noreply.github.com>
2021-10-08 14:11:47 +0300

    UIEH-1198: Jest+RTL > Test SettingsRoute (#1457)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,7 @@
 ## [7.1.0] (IN PROGRESS)
 
 * Fix store.getState is not a function error. (UIEH-1216)
+* Add tests for SettingsRoute component. (UIEH-1198)
 
 ## [7.0.0] (https://github.com/folio-org/ui-eholdings/tree/v7.0.0) (2021-10-06)
 
---
+++ src/routes/settings-route/settings-route.js
@@ -0,0 +1,51 @@
+import { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import ReactRouterPropTypes from 'react-router-prop-types';
+import { useIntl } from 'react-intl';
+
+import { TitleManager } from '@folio/stripes/core';
+
+import View from '../../components/settings';
+import ApplicationRoute from '../application';
+import { KbCredentials } from '../../constants';
+
+const SettingsRoute = ({
+  children,
+  getKbCredentials,
+  kbCredentials,
+  location,
+}) => {
+  const intl = useIntl();
+
+  useEffect(() => {
+    getKbCredentials();
+  }, []);
+
+  const sortKbCredentials = () => {
+    const kbCredentialsItems = kbCredentials.items;
+
+    return kbCredentialsItems.sort((a, b) => a.attributes.name.localeCompare(b.attributes.name));
+  };
+
+  return (
+    <ApplicationRoute showSettings>
+      <TitleManager page={intl.formatMessage({ id: 'ui-eholdings.label.settings' })}>
+        <View
+          location={location}
+          kbCredentials={sortKbCredentials()}
+        >
+          {children}
+        </View>
+      </TitleManager>
+    </ApplicationRoute>
+  );
+};
+
+SettingsRoute.propTypes = {
+  children: PropTypes.node.isRequired,
+  getKbCredentials: PropTypes.func.isRequired,
+  kbCredentials: KbCredentials.KbCredentialsReduxStateShape,
+  location: ReactRouterPropTypes.location.isRequired,
+};
+
+export default SettingsRoute;
