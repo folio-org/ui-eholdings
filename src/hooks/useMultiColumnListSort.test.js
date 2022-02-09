0e2c8152cc7419410c862127ed8e39ecedb43a22
Svitlana Korol <svitlana_korol@epam.com>
2022-02-09 17:02:52 +0200

    UIEH-1183: fixed comments

--- src/hooks/useMultiColumnListSort.test.js
+++ src/hooks/useMultiColumnListSort.test.js
@@ -20,27 +20,8 @@ describe('Given useMultiColumnListSort', () => {
     expect(sortParameters.sortedColumn).toBe('');
   });
 
-  describe('when handle "onChange" action', () => {
+  describe('when handle "onChange" action with the same name as currentColumn', () => {
     it('should return ascending sortOrder and selected sortedColumn', () => {
-      const currentParameters = {
-        sortOrder: {
-          name: 'asc',
-          fullName: 'ascending',
-        },
-        sortedColumn: 'newName',
-      };
-      const { result } = renderHook(() => useMultiColumnListSort(currentParameters.sortOrder, currentParameters.sortedColumn));
-      const [sortParameters, onChange] = result.current;
-
-      act(() => {
-        onChange('', { name: 'newName' });
-      });
-
-      expect(sortParameters.sortOrder).toEqual(sortOrders.asc);
-      expect(sortParameters.sortedColumn).toBe('newName');
-    });
-
-    it('should return descending sortOrder and selected sortedColumn', () => {
       const currentParameters = {
         sortOrder: {
           name: 'desc',
@@ -48,15 +29,42 @@ describe('Given useMultiColumnListSort', () => {
         },
         sortedColumn: 'newName',
       };
-      const { result } = renderHook(() => useMultiColumnListSort(currentParameters.sortOrder, currentParameters.sortedColumn));
-      const [sortParameters, onChange] = result.current;
+
+      const { result, rerender } = renderHook(() => useMultiColumnListSort(currentParameters.sortOrder, currentParameters.sortedColumn));
+      const onChange = result.current[1];
 
       act(() => {
         onChange('', { name: 'newName' });
       });
 
-      expect(sortParameters.sortOrder).toEqual(sortOrders.desc);
-      expect(sortParameters.sortedColumn).toBe('newName');
+      rerender();
+
+      expect(result.current[0].sortOrder).toEqual(sortOrders.asc);
+      expect(result.current[0].sortedColumn).toBe('newName');
+    });
+  });
+
+  describe('when handle "onChange" action with a different name as currentColumn', () => {
+    it('should set ascending sortOrder by default and selected sortedColumn', () => {
+      const currentParameters = {
+        sortOrder: {
+          name: 'asc',
+          fullName: 'ascending',
+        },
+        sortedColumn: 'newName',
+      };
+
+      const { result, rerender } = renderHook(() => useMultiColumnListSort(currentParameters.sortOrder, currentParameters.sortedColumn));
+      const onChange = result.current[1];
+
+      act(() => {
+        onChange('', { name: 'otherName' });
+      });
+
+      rerender();
+
+      expect(result.current[0].sortOrder).toEqual(sortOrders.asc);
+      expect(result.current[0].sortedColumn).toBe('otherName');
     });
   });
 });
