d82889b3d6dcf87ab60fccfc76838e03293b09be
Zak Burke <zburke@ebsco.com>
2022-02-17 15:04:46 -0500

    test config cleanup
    * in stripesComponents, mock `Icon` because it leverages webpack's
      `context` function, which node doesn't know about.
    * in stripesConfig, provide `metadata`, which was added to
      stripes-config long, long ago, but after this mock was originally
      written.
    * in babel.config.js, configure plugin-proposal-private-property-in-object
      consistently with others to avoid VERY NOISY console warnings.

--- test/jest/babel.config.js
+++ test/jest/babel.config.js
@@ -9,8 +9,17 @@ module.exports = {
   ],
   'plugins': [
     ['@babel/plugin-proposal-decorators', { 'legacy': true }],
+    // when building a platform directly, i.e. outside a workspace,
+    // babel complains loudly and repeatedly that when these modules are enabled:
+    // * @babel/plugin-proposal-class-properties,
+    // * @babel/plugin-proposal-private-methods and
+    // * @babel/plugin-proposal-private-property-in-object
+    // the "loose" option must be the same for all three.
+    // but @babel/preset-env sets it to false for ...private-methods.
+    // overriding it here silences the complaint. STRWEB-12
     ['@babel/plugin-proposal-class-properties', { 'loose': true }],
     ['@babel/plugin-proposal-private-methods', { 'loose': true }],
+    ['@babel/plugin-proposal-private-property-in-object', { 'loose': true }],
     '@babel/plugin-transform-runtime',
   ],
 };
