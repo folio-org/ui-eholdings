b6751fc3ae192ae0f5428028e49ba64daba4ae95
Denys Bohdan <Denys_Bohdan@epam.com>
2022-04-06 18:56:34 +0300

    Remove 'ky' from moduleNameMapper

--- jest.config.js
+++ jest.config.js
@@ -19,7 +19,6 @@ module.exports = {
   moduleNameMapper: {
     '^.+\\.(css)$': 'identity-obj-proxy',
     '^.+\\.(svg)$': 'identity-obj-proxy',
-    'ky': 'ky/umd',
   },
   testMatch: ['**/(lib|src)/**/?(*.)test.{js,jsx}'],
   testPathIgnorePatterns: ['/node_modules/', '/test/ui-testing/', '/test/bigtest/'],
