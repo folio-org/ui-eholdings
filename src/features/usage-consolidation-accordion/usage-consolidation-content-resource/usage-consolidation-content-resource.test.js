e4fd9a4f9865472143ec6718d4e578e6dadf07a2
Priyanka <priyanka_terala@epam.com>
2022-04-28 16:05:19 +0530

    fixed GitHub Actions CI check failures

--- src/features/usage-consolidation-accordion/usage-consolidation-content-resource/usage-consolidation-content-resource.test.js
+++ src/features/usage-consolidation-accordion/usage-consolidation-content-resource/usage-consolidation-content-resource.test.js
@@ -1,118 +1,116 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import Harness from '../../../../test/jest/helpers/harness';
 
 import UsageConsolidationContentResource from './usage-consolidation-content-resource';
-import {
-    costPerUseTypes
-  } from '../../../constants';
+import { costPerUseTypes } from '../../../constants';
 
 const costPerUseData = {
-    data: {
-        [costPerUseTypes.RESOURCE_COST_PER_USE]:{
-            attributes: {
-                analysis: {
-                    cost: 1,
-                    costPerUse: 1,
-                    usage: 1
-                },
-                parameters: {
-                    currency: '',
-                    startMonth: '',
-                },
-                usage:{
-                    platforms:[
-                        {
-                            counts: [3, 6, 16],
-                            isPublisherPlatform: true,
-                            name: "name",
-                            total: 17
-                        }
-                    ],
-                    totals:{}
-                }
-            },
+  data: {
+    [costPerUseTypes.RESOURCE_COST_PER_USE]:{
+      attributes: {
+        analysis: {
+            cost: 1,
+            costPerUse: 1,
+            usage: 1
         },
-    },
-    isFailed: false,
-    isLoaded: true,
-    isLoading: false,
+        parameters: {
+            currency: '',
+            startMonth: '',
+        },
+        usage:{
+            platforms:[
+                {
+                    counts: [3, 6, 16],
+                    isPublisherPlatform: true,
+                    name: "name",
+                    total: 17
+                }
+            ],
+            totals:{}
+          }
+        },
+      },
+  },
+  isFailed: false,
+  isLoaded: true,
+  isLoading: false,
 };
 
 const renderUsageConsolidationContentResource = (props = {}) => render(
-    <Harness>
-      <UsageConsolidationContentResource
-        costPerUseData={costPerUseData}
-        {...props}
-      />
-    </Harness>
-  );
+  <Harness>
+    <UsageConsolidationContentResource
+      costPerUseData={costPerUseData}
+      {...props}
+    />
+  </Harness>
+);
 
 describe("Given UsageConsolidationContentResource", () => {
 
-    it('No DOMelement should be rendered when costPerUseData.data.resourceCostPerUse is null', () => {
-        const newCostPerUseData = {
-            ...costPerUseData,
-            data: {
-                ...costPerUseData.data,
-                [costPerUseTypes.RESOURCE_COST_PER_USE]: null
-            }
-        };
+  it('No DOMelement should be rendered when costPerUseData.data.resourceCostPerUse is null', () => {
+    const newCostPerUseData = {
+      ...costPerUseData,
+      data: {
+        ...costPerUseData.data,
+        [costPerUseTypes.RESOURCE_COST_PER_USE]: null
+      }
+    };
 
-        const {container} = renderUsageConsolidationContentResource({
-            costPerUseData: newCostPerUseData
-        });
-
-        expect(container).toBeEmptyDOMElement;
+    const {container} = renderUsageConsolidationContentResource({
+      costPerUseData: newCostPerUseData
     });
 
-    it('should render a div with message when isFailed of costPerUseData is true', () => {
-        const newCostPerUseData = {
-            ...costPerUseData,
-            isFailed: true
-        };
-        const { getByText } = renderUsageConsolidationContentResource({
-            costPerUseData: newCostPerUseData
-        });
+    expect(container).toBeEmptyDOMElement;
+  });
 
-        expect(getByText('ui-eholdings.usageConsolidation.fullTextRequestUsageTable.noResponse'));
+  it('should render a div with message when isFailed of costPerUseData is true', () => {
+    const newCostPerUseData = {
+      ...costPerUseData,
+      isFailed: true
+    };
+    const { getByText } = renderUsageConsolidationContentResource({
+      costPerUseData: newCostPerUseData
     });
 
-    it('should render NoCostPerUseAvailable when noCostPerUseAvailable and isPlatformsDataEmpty are true', () => {
-        const newCostPerUseData = {
-            ...costPerUseData,
-            data:{
-                ...costPerUseData.data,
-                [costPerUseTypes.RESOURCE_COST_PER_USE] : {
-                    ...costPerUseData.data[costPerUseTypes.RESOURCE_COST_PER_USE],
-                    attributes: {
-                        analysis: {
-                            cost: 0,
-                            costPerUse: 0,
-                            usage: 0
-                        },
-                        usage: {
-                            platforms: [],
-                            totals: {}
-                        }
-                    }
-                }
+    expect(getByText('ui-eholdings.usageConsolidation.fullTextRequestUsageTable.noResponse'));
+  });
+
+  it('should render NoCostPerUseAvailable when noCostPerUseAvailable and isPlatformsDataEmpty are true', () => {
+    const newCostPerUseData = {
+      ...costPerUseData,
+      data:{
+        ...costPerUseData.data,
+        [costPerUseTypes.RESOURCE_COST_PER_USE] : {
+          ...costPerUseData.data[costPerUseTypes.RESOURCE_COST_PER_USE],
+          attributes: {
+            analysis: {
+              cost: 0,
+              costPerUse: 0,
+              usage: 0
+            },
+            usage: {
+                platforms: [],
+                totals: {}
             }
-        };
-        const { getByText } = renderUsageConsolidationContentResource({
-            costPerUseData: newCostPerUseData
-        });
+          }
+        }
+      }
+    };
+    const { getByText } = renderUsageConsolidationContentResource({
+      costPerUseData: newCostPerUseData
+    });
     
-        expect(getByText('ui-eholdings.usageConsolidation.summary.resource.noData')).toBeDefined();
-    });
+    expect(getByText('ui-eholdings.usageConsolidation.summary.resource.noData')).toBeDefined();
+  });
 
-    it('render Summary Table and FullTextRequestUsageTable when costPerUseData hold appropriate values', () => {
-        const {getByText} = renderUsageConsolidationContentResource();
+  it('render Summary Table and FullTextRequestUsageTable when costPerUseData hold appropriate values', () => {
+    const {getByText} = renderUsageConsolidationContentResource();
         
-        // SummaryTable
-        expect(getByText('ui-eholdings.usageConsolidation.summary.costPerUse')).toBeDefined();
+    // SummaryTable
+    expect(getByText('ui-eholdings.usageConsolidation.summary.costPerUse')).toBeDefined();
 
-        // FullTextRequestUsageTable
-        expect(getByText('ui-eholdings.usageConsolidation.fullTextRequestUsageTable')).toBeDefined();
-    });
+    // FullTextRequestUsageTable
+    expect(getByText('ui-eholdings.usageConsolidation.fullTextRequestUsageTable')).toBeDefined();
+  });
 });
