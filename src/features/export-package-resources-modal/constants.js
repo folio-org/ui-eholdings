7d261755a893faf97691bbe2fe94618cbbef67b7
Dmytro-Melnyshyn <Dmytro_Melnyshyn@epam.com>
2022-10-13 13:44:43 +0300

    UIEH-1316: remove 'Provider Level Token' from Title fields

--- src/features/export-package-resources-modal/constants.js
+++ src/features/export-package-resources-modal/constants.js
@@ -10,13 +10,7 @@ export const RECORD_TYPES = {
   RESOURCE: 'resource',
 };
 
-const PROVIDER_LEVEL_TOKEN = {
-  value: 'providerLevelToken',
-  label: 'ui-eholdings.exportPackageResources.fields.package.providerLevelToken'
-};
-
 const RESOURCE_EXPORT_FIELDS = [
-  PROVIDER_LEVEL_TOKEN,
   {
     value: 'titleName',
     label: 'ui-eholdings.exportPackageResources.fields.title.titleName'
@@ -136,7 +130,10 @@ const RESOURCE_EXPORT_FIELDS = [
 ];
 
 const PACKAGE_EXPORT_FIELDS = [
-  PROVIDER_LEVEL_TOKEN,
+  {
+    value: 'providerLevelToken',
+    label: 'ui-eholdings.exportPackageResources.fields.package.providerLevelToken'
+  },
   {
     value: 'providerName',
     label: 'ui-eholdings.exportPackageResources.fields.package.providerName'
@@ -207,7 +204,7 @@ export const FIELDS_BY_RECORD_TYPE = {
 // some fields, like customLabels should be sent to backend like several combined fields: customValue1, customValue2 etc
 export const PAYLOAD_READY_FIELDS_BY_RECORD_TYPE = {
   [RECORD_TYPES.PACKAGE]: {
-    providerLevelToken: () => [PROVIDER_LEVEL_TOKEN.value],
+    providerLevelToken: () => ['providerLevelToken'],
     providerName: () => ['providerName'],
     providerId: () => ['providerId'],
     packageName: () => ['packageName'],
@@ -225,7 +222,6 @@ export const PAYLOAD_READY_FIELDS_BY_RECORD_TYPE = {
     packageNotes: () => ['packageNotes'],
   },
   [RECORD_TYPES.RESOURCE]: {
-    providerLevelToken: () => [PROVIDER_LEVEL_TOKEN.value],
     titleName: () => ['titleName'],
     alternateTitles: () => ['alternateTitles'],
     titleId: () => ['titleId'],
