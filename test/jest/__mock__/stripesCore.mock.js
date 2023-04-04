6e6430ceb96e4e7ccfea3caeae1066022d265ea2
Michal Kuklis <michal.kuklis@gmail.com>
2023-04-04 15:05:07 -0400

    Adjust tests

--- test/jest/__mock__/stripesCore.mock.js
+++ test/jest/__mock__/stripesCore.mock.js
@@ -39,7 +39,7 @@ const buildStripes = (otherProperties = {}) => ({
   ...otherProperties,
 });
 
-jest.mock('@folio/stripes-core', () => {
+jest.mock('@folio/stripes/core', () => {
   const STRIPES = buildStripes();
 
   // eslint-disable-next-line react/prop-types
@@ -89,7 +89,7 @@ jest.mock('@folio/stripes-core', () => {
   STRIPES.connect = stripesConnect;
 
   return {
-    ...jest.requireActual('@folio/stripes-core'),
+    ...jest.requireActual('@folio/stripes/core'),
     stripesConnect,
     withStripes,
     IfPermission,
