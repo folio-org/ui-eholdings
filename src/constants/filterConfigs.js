889afb667d46c19b8deaf456393e16f24b695fa3
Ryan Lee <rlee1@ebsco.com>
2026-08-12 11:35:19 -0700

    UIEH-1527: Update content type control type and filter behavior (#1836)

--- src/constants/filterConfigs.js
+++ src/constants/filterConfigs.js
@@ -9,16 +9,16 @@ export const baseSortFilterConfig = {
 export const providerSortFilterConfig = {
   ...baseSortFilterConfig,
   options: [
-    { label: <FormattedMessage id="ui-eholdings.filter.sortOptions.relevance" />, value: 'relevance' },
-    { label: <FormattedMessage id="ui-eholdings.label.provider" />, value: 'name' }
+    { labelId: 'ui-eholdings.filter.sortOptions.relevance', value: 'relevance' },
+    { labelId: 'ui-eholdings.label.provider', value: 'name' }
   ],
 };
 
 export const packageSortFilterConfig = {
   ...baseSortFilterConfig,
   options: [
-    { label: <FormattedMessage id="ui-eholdings.filter.sortOptions.relevance" />, value: 'relevance' },
-    { label: <FormattedMessage id="ui-eholdings.label.package" />, value: 'name' }
+    { labelId: 'ui-eholdings.filter.sortOptions.relevance', value: 'relevance' },
+    { labelId: 'ui-eholdings.label.package', value: 'name' }
   ],
 };
 
@@ -34,6 +34,7 @@ export const selectionStatusDefaultFilterOption = selectionStatusFilterOptions.A
 
 export const FILTER_TYPES = {
   SELECT: 'select',
+  SELECTION: 'selection',
   CHECKBOX: 'checkbox',
 };
 
@@ -44,15 +45,15 @@ export const selectionStatusFilterConfig = {
   defaultValue: selectionStatusDefaultFilterOption,
   options: [
     {
-      label: <FormattedMessage id="ui-eholdings.filter.all" />,
+      labelId: 'ui-eholdings.filter.all',
       value: selectionStatusFilterOptions.ALL,
     },
     {
-      label: <FormattedMessage id="ui-eholdings.selected" />,
+      labelId: 'ui-eholdings.selected',
       value: selectionStatusFilterOptions.TRUE,
     },
     {
-      label: <FormattedMessage id="ui-eholdings.notSelected" />,
+      labelId: 'ui-eholdings.notSelected',
       value: selectionStatusFilterOptions.FALSE,
     },
   ]
@@ -64,47 +65,47 @@ export const publicationTypeTitlesListFilterConfig = {
   label: <FormattedMessage id="ui-eholdings.label.publicationType" />,
   defaultValue: 'all',
   options: [
-    { label: <FormattedMessage id="ui-eholdings.filter.all" />, value: 'all' },
-    { label: <FormattedMessage id="ui-eholdings.filter.pubType.audiobook" />, value: 'audiobook' },
-    { label: <FormattedMessage id="ui-eholdings.filter.pubType.book" />, value: 'book' },
-    { label: <FormattedMessage id="ui-eholdings.filter.pubType.book_series" />, value: 'bookseries' },
-    { label: <FormattedMessage id="ui-eholdings.filter.pubType.database" />, value: 'database' },
-    { label: <FormattedMessage id="ui-eholdings.filter.pubType.journal" />, value: 'journal' },
-    { label: <FormattedMessage id="ui-eholdings.filter.pubType.newsletter" />, value: 'newsletter' },
-    { label: <FormattedMessage id="ui-eholdings.filter.pubType.newspaper" />, value: 'newspaper' },
-    { label: <FormattedMessage id="ui-eholdings.filter.pubType.proceedings" />, value: 'proceedings' },
-    { label: <FormattedMessage id="ui-eholdings.filter.pubType.report" />, value: 'report' },
-    { label: <FormattedMessage id="ui-eholdings.filter.pubType.streaming_audio" />, value: 'streamingaudio' },
-    { label: <FormattedMessage id="ui-eholdings.filter.pubType.streaming_video" />, value: 'streamingvideo' },
-    { label: <FormattedMessage id="ui-eholdings.filter.pubType.thesis_dissertation" />, value: 'thesisdissertation' },
-    { label: <FormattedMessage id="ui-eholdings.filter.pubType.website" />, value: 'website' },
-    { label: <FormattedMessage id="ui-eholdings.filter.pubType.unspecified" />, value: 'unspecified' }
+    { labelId: 'ui-eholdings.filter.all', value: 'all' },
+    { labelId: 'ui-eholdings.filter.pubType.audiobook', value: 'audiobook' },
+    { labelId: 'ui-eholdings.filter.pubType.book', value: 'book' },
+    { labelId: 'ui-eholdings.filter.pubType.book_series', value: 'bookseries' },
+    { labelId: 'ui-eholdings.filter.pubType.database', value: 'database' },
+    { labelId: 'ui-eholdings.filter.pubType.journal', value: 'journal' },
+    { labelId: 'ui-eholdings.filter.pubType.newsletter', value: 'newsletter' },
+    { labelId: 'ui-eholdings.filter.pubType.newspaper', value: 'newspaper' },
+    { labelId: 'ui-eholdings.filter.pubType.proceedings', value: 'proceedings' },
+    { labelId: 'ui-eholdings.filter.pubType.report', value: 'report' },
+    { labelId: 'ui-eholdings.filter.pubType.streaming_audio', value: 'streamingaudio' },
+    { labelId: 'ui-eholdings.filter.pubType.streaming_video', value: 'streamingvideo' },
+    { labelId: 'ui-eholdings.filter.pubType.thesis_dissertation', value: 'thesisdissertation' },
+    { labelId: 'ui-eholdings.filter.pubType.website', value: 'website' },
+    { labelId: 'ui-eholdings.filter.pubType.unspecified', value: 'unspecified' }
   ]
 };
 
 export const titleSortFilterConfig = {
   ...baseSortFilterConfig,
   options: [
-    { label: <FormattedMessage id="ui-eholdings.filter.sortOptions.relevance" />, value: 'relevance' },
-    { label: <FormattedMessage id="ui-eholdings.label.title" />, value: 'name' }
+    { labelId: 'ui-eholdings.filter.sortOptions.relevance', value: 'relevance' },
+    { labelId: 'ui-eholdings.label.title', value: 'name' }
   ],
 };
 
 export const contentTypeFilterConfig = {
-  type: FILTER_TYPES.CHECKBOX,
+  type: FILTER_TYPES.SELECTION,
   name: 'type',
   label: <FormattedMessage id="ui-eholdings.package.contentType" />,
   defaultValue: 'all',
   options: [
-    { label: <FormattedMessage id="ui-eholdings.filter.all" />, value: 'all' },
-    { label: <FormattedMessage id="ui-eholdings.filter.contentType.aggregated" />, value: 'aggregatedfulltext' },
-    { label: <FormattedMessage id="ui-eholdings.filter.contentType.abstract" />, value: 'abstractandindex' },
-    { label: <FormattedMessage id="ui-eholdings.filter.contentType.ebook" />, value: 'ebook' },
-    { label: <FormattedMessage id="ui-eholdings.filter.contentType.ejournal" />, value: 'ejournal' },
-    { label: <FormattedMessage id="ui-eholdings.filter.contentType.mixed_content" />, value: 'mixedcontent' },
-    { label: <FormattedMessage id="ui-eholdings.filter.contentType.online_reference" />, value: 'onlinereference' },
-    { label: <FormattedMessage id="ui-eholdings.filter.contentType.print" />, value: 'print' },
-    { label: <FormattedMessage id="ui-eholdings.filter.contentType.streaming_media" />, value: 'streamingmedia' },
-    { label: <FormattedMessage id="ui-eholdings.filter.contentType.unknown" />, value: 'unknown' }
+    { labelId: 'ui-eholdings.filter.all', value: 'all' },
+    { labelId: 'ui-eholdings.filter.contentType.aggregated', value: 'aggregatedfulltext' },
+    { labelId: 'ui-eholdings.filter.contentType.abstract', value: 'abstractandindex' },
+    { labelId: 'ui-eholdings.filter.contentType.ebook', value: 'ebook' },
+    { labelId: 'ui-eholdings.filter.contentType.ejournal', value: 'ejournal' },
+    { labelId: 'ui-eholdings.filter.contentType.mixed_content', value: 'mixedcontent' },
+    { labelId: 'ui-eholdings.filter.contentType.online_reference', value: 'onlinereference' },
+    { labelId: 'ui-eholdings.filter.contentType.print', value: 'print' },
+    { labelId: 'ui-eholdings.filter.contentType.streaming_media', value: 'streamingmedia' },
+    { labelId: 'ui-eholdings.filter.contentType.unknown', value: 'unknown' }
   ]
 };
--- src/routes/provider-show-route/package-search-filters-config.js
+++ src/routes/provider-show-route/package-search-filters-config.js
@@ -8,15 +8,15 @@ export const contentTypeFilterConfig = {
   label: <FormattedMessage id="ui-eholdings.package.contentType" />,
   defaultValue: 'all',
   options: [
-    { label: <FormattedMessage id="ui-eholdings.filter.all" />, value: 'all' },
-    { label: <FormattedMessage id="ui-eholdings.filter.contentType.aggregated" />, value: 'aggregatedfulltext' },
-    { label: <FormattedMessage id="ui-eholdings.filter.contentType.abstract" />, value: 'abstractandindex' },
-    { label: <FormattedMessage id="ui-eholdings.filter.contentType.ebook" />, value: 'ebook' },
-    { label: <FormattedMessage id="ui-eholdings.filter.contentType.ejournal" />, value: 'ejournal' },
-    { label: <FormattedMessage id="ui-eholdings.filter.contentType.mixed_content" />, value: 'mixedcontent' },
-    { label: <FormattedMessage id="ui-eholdings.filter.contentType.online_reference" />, value: 'onlinereference' },
-    { label: <FormattedMessage id="ui-eholdings.filter.contentType.print" />, value: 'print' },
-    { label: <FormattedMessage id="ui-eholdings.filter.contentType.streaming_media" />, value: 'streamingmedia' },
-    { label: <FormattedMessage id="ui-eholdings.filter.contentType.unknown" />, value: 'unknown' }
+    { labelId: 'ui-eholdings.filter.all', value: 'all' },
+    { labelId: 'ui-eholdings.filter.contentType.aggregated', value: 'aggregatedfulltext' },
+    { labelId: 'ui-eholdings.filter.contentType.abstract', value: 'abstractandindex' },
+    { labelId: 'ui-eholdings.filter.contentType.ebook', value: 'ebook' },
+    { labelId: 'ui-eholdings.filter.contentType.ejournal', value: 'ejournal' },
+    { labelId: 'ui-eholdings.filter.contentType.mixed_content', value: 'mixedcontent' },
+    { labelId: 'ui-eholdings.filter.contentType.online_reference', value: 'onlinereference' },
+    { labelId: 'ui-eholdings.filter.contentType.print', value: 'print' },
+    { labelId: 'ui-eholdings.filter.contentType.streaming_media', value: 'streamingmedia' },
+    { labelId: 'ui-eholdings.filter.contentType.unknown', value: 'unknown' }
   ]
 };
