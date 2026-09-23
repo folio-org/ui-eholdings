aba2d9a63ac17040007efb8a4696df18f4a5869f
anupamme <mediratta@gmail.com>
2026-09-23 12:53:55 +0000

    fix: V-001 security vulnerability
    Automated security fix generated by OrbisAI Security

--- src/redux/data.js
+++ src/redux/data.js
@@ -581,7 +581,7 @@ export function epic(action$, state$) {
       }
 
       // request which rejects when not OK
-      const promise = fetch(url, { headers, method, body, credentials: 'include' })
+      const promise = fetch(url, { headers, method, body, credentials: 'same-origin' })
         .then(response => Promise.all([response.ok, parseResponseBody(response), response.status]))
         .then(([ok, responseBody, status]) => (ok
           ? { status, responseBody }
